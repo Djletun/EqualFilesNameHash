46e4627db9640a0262b0afc26e852031667bbc7b
Djletun <djletun@tut.by>
2022-09-10 04:15:40 -0400

    3. вывод результата в файл, ошибочное glob.glob вывод в файл.

--- main.py
+++ main.py
@@ -3,11 +3,7 @@
 import glob
 import sys
 import os
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from hashlib import md5
 
 
 def symbol():
@@ -26,6 +22,7 @@ def mk_file_list():
             target_path += smbl  # добавляем в конец пути к директории слеш - будет искать во всех подпапках
         # print(target_path + '**' + smbl + '*' + file_types)
         file_list_dir.extend(glob.glob(target_path + '**' + smbl + '*' + file_types, recursive=True))
+    # print(file_list_dir)
     set_fl_lst_dr = set(file_list_dir)  # исключаю повторы путей файлов
     return list(set_fl_lst_dr)
 
@@ -34,6 +31,7 @@ def mk_dict_equal_names(unic_name_list, name_list, file_list_dir):
     result = dict()
     for file_name in unic_name_list:
         # первое вхождение file_name в списке
+
         first_indx = name_list.index(file_name)
         if file_name in name_list[first_indx + 1:]:
             result_set = set()
@@ -48,6 +46,7 @@ def mk_dict_equal_names(unic_name_list, name_list, file_list_dir):
 
     return result
 
+
 def mk_dict_equal_sizes(result):
     keys = list(result.keys())
     for k in keys:
@@ -55,7 +54,7 @@ def mk_dict_equal_sizes(result):
         for file_name in result[k]:
             counter.append([os.path.getsize(file_name), file_name])
 
-        #print(counter)
+        # print(counter)
         sizes_lst = list(map(lambda x: x[0], counter))
         # корректирую список оставляю только одинаковые размеры
         for itm in set(sizes_lst):
@@ -64,21 +63,64 @@ def mk_dict_equal_sizes(result):
             elif sizes_lst.count(itm) == 1:
                 tmp = (list(map(lambda x: x[0], counter))).index(itm)
                 counter.pop(tmp)
-        #корректирую результирующий словарь
+        # корректирую результирующий словарь
         if len(counter) == 0:
-            #result[k].clear()
+            # result[k].clear()
             del result[k]
         elif len(counter) != len(result[k]):
             result[k] = list(map(lambda x: x[1], counter))
     return result
 
 
+def error_files(txt):
+    with open('err.txt', 'a+') as err_f:
+        err_f.write(txt + '\n')
+        err_f.close()
+
+
+def mk_dict_equal_hashes(result):
+    keys = list(result.keys())
+    for k in keys:
+        digests = list()
+        for File_Nm in result[k]:
+
+            try:
+                with open(File_Nm, 'rb') as f:
+                    digest = md5(f.read()).hexdigest()
+                    digests.append([digest, File_Nm])
+
+            except Exception:
+                print(File_Nm)
+                error_files(File_Nm)
+                print(k)
+                error_files(k)
+
+                break
+
+        # print(digests)
+        md5_lst = list(map(lambda x: x[0], digests))
+        # корректирую список оставляю только одинаковые хэши']
+        for itm in set(md5_lst):
+            if len(digests) == 1:
+                digests.clear()
+            elif md5_lst.count(itm) == 1:
+                tmp = (list(map(lambda x: x[0], digests))).index(itm)
+                digests.pop(tmp)
+        # корректирую результирующий словарь
+        if len(digests) == 0:
+            # result[k].clear()
+            del result[k]
+        elif len(digests) != len(result[k]):
+            result[k] = list(map(lambda x: x[1], digests))
+    return result
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     smbl = symbol()
-    #sys.argv = ['~/PycharmProjects/EqualFilesNameHash/main.py', '/home/me/PycharmProjects/EqualFilesNameHash/samples/',
-    #            '.txt']
-    #sys.argv = ['F:\home\me\PycharmProjects\EqualFilesNameHash\main.py', 'D:\SERCEL', '.pdf']
+    sys.argv = ['~/PycharmProjects/EqualFilesNameHash/main.py', '/media/me/win10/MY/',
+                '.*']
+    # sys.argv = ['F:\home\me\PycharmProjects\EqualFilesNameHash\main.py', 'D:\doc-i\работа', '.*']
     # получаю список путей файлов
     file_list_dir = mk_file_list()
     name_list = []  # только имена файлов
@@ -86,16 +128,24 @@ if __name__ == '__main__':
     for file_name in file_list_dir:
         name_list.append(file_name.split(smbl)[-1])
 
-    unic_name_list = []  # список с уникальными/неповторимыми именами файлов
+    # unic_name_list = []  # список с уникальными/неповторимыми именами файлов
     unic_name_list = list(set(name_list))
     # получить словарь: ключ - имя файла, и полные пути до файлов с одинаковыми именами
     result = mk_dict_equal_names(unic_name_list, name_list, file_list_dir)
-    print(*result)
+    print('1')
+    # print(*result)
     # получить словарь: ключ - имя файла, и полные пути до файлов с одинаковыми именами и размерами
     result = mk_dict_equal_sizes(result)
-    print(*result)
+    print('2')
+    # print(*result)
+    result = mk_dict_equal_hashes(result)
+
     # печатаем красиво
-    for k in result.keys():
-        print(k)
-        for n in result[k]:
-            print('\t',n,'\t', os.path.getsize(n))
+
+    with open('result.txt', 'w') as f:
+        for k in result.keys():
+            # for k in sorted(result, key=lambda ) .keys():
+            print(k, file=f)
+            for n in result[k]:
+                print('\t', n, '\t', os.path.getsize(n), file=f)
+        f.close()
